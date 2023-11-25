c10b831865cd0751a28f39b798bdb820bc7b4880
Амир Кахаров <amir.kakharov2001@gmail.com>
2023-11-25 04:30:04 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,56 @@
-﻿namespace Algorythm
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class User
 {
-    internal class Program
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public bool IsPremium { get; set; }
+}
+
+class Program
+{
+    static void Main()
     {
-        static void Main(string[] args)
+        // Ввод списка пользователей
+        List<User> users = new List<User>
         {
-            Console.WriteLine("Hello, World!");
+            new User { Login = "user1", Name = "John", IsPremium = true },
+            new User { Login = "user2", Name = "Alice", IsPremium = false },
+            new User { Login = "user3", Name = "Bob", IsPremium = true }
+        };
+
+        // Для каждого пользователя
+        foreach (User user in users)
+        {
+            // Приветствие по имени
+            GreetUser(user);
+
+            // Если премиум подписка, пропустить рекламу, иначе показать
+            if (!user.IsPremium)
+            {
+                ShowAds();
+            }
         }
+    }
+
+    // Метод для приветствия пользователя
+    static void GreetUser(User user)
+    {
+        Console.WriteLine($"Привет, {user.Name}!");
+    }
+
+    // Метод для показа рекламы
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
     }
 }
